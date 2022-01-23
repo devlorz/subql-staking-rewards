d096686be1c02905c382048bd1367ccf964079fa
Nutti Saelor <leelorz6@gmail.com>
2022-01-23 16:38:32 +0700

    Finish lesson 2

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,6 +1,12 @@
 import { Balance } from "@polkadot/types/interfaces";
 import { SubstrateEvent } from "@subql/types";
-import { StakingReward } from "../types";
+import { StakingReward, SumReward } from "../types";
+
+function createSumReward(accountId: string): SumReward {
+  const entity = new SumReward(accountId);
+  entity.totalReward = BigInt(0);
+  return entity;
+}
 
 export async function handleStakingRewarded(
   event: SubstrateEvent
@@ -17,5 +23,20 @@ export async function handleStakingRewarded(
   entity.balance = (newReward as Balance).toBigInt();
   entity.date = event.block.timestamp;
   entity.blockHeight = event.block.block.header.number.toNumber();
+  await entity.save();
+}
+
+export async function handleSumRewarded(event: SubstrateEvent): Promise<void> {
+  const {
+    event: {
+      data: [account, newReward],
+    },
+  } = event;
+  let entity = await SumReward.get(account.toString());
+  if (entity === undefined) {
+    entity = createSumReward(account.toString());
+  }
+  entity.totalReward = entity.totalReward + (newReward as Balance).toBigInt();
+  entity.blockheight = event.block.block.header.number.toNumber();
   await entity.save();
 }
